4dbac129a27f7b1472cb511e3bb90d5706d2892d
hambuger233 <55654324+hambuger233@users.noreply.github.com>
2022-05-10 19:07:47 +0800

    Update SMSUtils.java

--- src/main/java/com/itheima/reggie/utils/SMSUtils.java
+++ src/main/java/com/itheima/reggie/utils/SMSUtils.java
@@ -31,7 +31,7 @@ public class SMSUtils {
          * 你也可以直接在代码中写死密钥对，但是小心不要将代码复制、上传或者分享给他人，
          * 以免泄露密钥对危及你的财产安全。
          * SecretId、SecretKey 查询: https://console.cloud.tencent.com/cam/capi */
-        Credential cred = new Credential("AKIDDE0UjhNmumNhkY4JGxNaWBaiY7Qp3231", "nV9j8G8pR3atvhCQN3SaR1KM7bokPr41");
+        Credential cred = new Credential(, );
 
         // 实例化一个http选项，可选，没有特殊需求可以跳过
         HttpProfile httpProfile = new HttpProfile();
